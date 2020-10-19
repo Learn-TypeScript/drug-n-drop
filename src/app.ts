13408f2c8b749ca822b263ebcdee0616a7a8d727
fotis tsakiris <footios76@gmail.com>
2020-10-19 09:14:01 +0300

    validation and ListClass

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,28 @@
+type validate = {
+  value: string | number;
+  required: boolean;
+  minLength?: number;
+  min?: number;
+  max?: number;
+};
+
+function validation(obj: validate) {
+  let isValid = true;
+  if (typeof obj.required === "boolean" && obj.required) {
+    isValid = isValid && obj.value.toString().length > 0;
+  }
+  if (typeof obj.value === "string" && obj.minLength) {
+    isValid = isValid && obj.value.length > obj.minLength;
+  }
+  if (typeof obj.value === "number" && obj.min) {
+    isValid = isValid && obj.value > obj.min;
+  }
+  if (typeof obj.value === "number" && obj.max) {
+    isValid = isValid && obj.value < obj.max;
+  }
+  return isValid;
+}
+
 function Autobind(_: any, _2: any, desc: PropertyDescriptor) {
   return {
     get() {
@@ -5,6 +30,40 @@ function Autobind(_: any, _2: any, desc: PropertyDescriptor) {
     }
   };
 }
+
+class ListClass {
+  templateElem: HTMLTemplateElement;
+  hostElem: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(public type: "active" | "finished") {
+    this.templateElem = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElem = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(this.templateElem.content, true);
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent = this.type.toUpperCase();
+    // this.element.querySelector("h3")!.textContent = this.type.toUpperCase();
+    // this.element.querySelector("p")!.textContent = this.type.toUpperCase();
+  }
+
+  private attach() {
+    this.hostElem.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
+const list = new ListClass("active");
+const list1 = new ListClass("finished");
 
 class InputClass {
   templateElem: HTMLTemplateElement;
@@ -30,10 +89,46 @@ class InputClass {
     this.people = this.element.querySelector("#people") as HTMLInputElement;
     this.desc = this.element.querySelector("#description") as HTMLInputElement;
   }
+
+  private clearInputs() {
+    this.title.value = "";
+    this.people.value = "";
+    this.desc.value = "";
+  }
+
+  private getUserInput(): [string, number, string] | void {
+    const title = this.title.value;
+    const people = this.people.value;
+    const desc = this.desc.value;
+
+    const validTitle = { value: title, minLength: 3, required: true };
+    const validPeople = { value: +people, min: 1, max: 10, required: true };
+    const validDesc = { value: desc, minLength: 5, required: true };
+
+    if (
+      validation(validTitle) &&
+      validation(validPeople) &&
+      validation(validDesc)
+    ) {
+      return [title, +people, desc];
+    } else {
+      alert("No valid input...");
+      return;
+    }
+  }
+
   @Autobind
   private submitHandler(e: Event) {
     e.preventDefault();
-    console.log(this.title.value);
+    const userInput = this.getUserInput();
+    console.log(userInput);
+    // Without this if check we get the following error:
+    // Type 'void | [string, number, string]' must have a '[Symbol.iterator]()' method that returns an iterator.
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, people, desc);
+    }
+    this.clearInputs();
   }
 
   private configure() {
